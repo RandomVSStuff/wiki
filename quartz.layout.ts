ca12c36f63c2db050ddd9feaf0301865c75b4e1a
Christian Cox <31042139+BillyBobCox@users.noreply.github.com>
2024-12-20 22:30:43 -0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://randomvsstuff.github.io/wiki/",
+      "Discord Community": "https://discord.gg/QrGBeDWcfC",
     },
   }),
 }
